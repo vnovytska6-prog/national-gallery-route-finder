00e7720c3213f802ec6e1961f85ee788153bf6a9
SeanMatthews20111328 <20111328@mail.wit.ie>
2026-05-08 22:19:01 +0100

    Filled out Algorithms in RouteFinder class. Added List<GraphNode> to GraphNode 08/05/26

--- main/java/gallery/routefinder/graph/GraphNode.java
+++ main/java/gallery/routefinder/graph/GraphNode.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class GraphNode {
     private Room room;
     private List<GraphEdge> edges;
+    private List<GraphNode> nodes;
     private double distanceFromStart;
     private boolean visited;
     private GraphNode previous;
@@ -14,6 +15,7 @@ public class GraphNode {
     public GraphNode(Room room) {
         this.room = room;
         this.edges = new ArrayList<>();
+        this.nodes = new ArrayList<>();
         this.distanceFromStart = Double.POSITIVE_INFINITY;
         this.visited = false;
         this.previous = null;
@@ -26,6 +28,7 @@ public class GraphNode {
     // Getters and setters
     public Room getRoom() { return room; }
     public List<GraphEdge> getEdges() { return edges; }
+    public List<GraphNode> getNodes() { return nodes; }
     public double getDistanceFromStart() { return distanceFromStart; }
     public void setDistanceFromStart(double distance) { this.distanceFromStart = distance; }
     public boolean isVisited() { return visited; }
